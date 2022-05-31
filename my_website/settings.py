9f13acd6b84d29c57ebb39f1650d062ddb86ea94
Akiramohanad <tbdra8on@gmail.com>
2022-06-01 00:35:10 +0300

    static

--- my_website/settings.py
+++ my_website/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
-STATICFILES_DIRS = [
-    # BASE_DIR / "static",
-    BASE_DIR / "my_website/static/",
-]
+# STATICFILES_DIRS = [
+#     # BASE_DIR / "static",
+#     BASE_DIR / "my_website/static/",
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
